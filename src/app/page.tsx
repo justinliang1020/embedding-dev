8153dec3e14bcd6859f724b342af4d6aaabf6185
Justin Liang <justinliang1020@gmail.com>
2023-06-05 16:37:37 -0700

    added header with daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { FormEvent, useState } from "react";
 import { v4 as uuidv4, v4 } from "uuid";
 import { Output, models } from "@/utils/utils";
-
+import Header from "../components/Header";
 export default function Home() {
   const [outputs, setOutputs] = useState<{ [key: string]: Output[] }>({})
   const [query, setQuery] = useState("");
@@ -18,52 +18,53 @@ export default function Home() {
     setOutputs(await res.json());
   }
   return (
-    <main className="flex h-screen flex-col items-center space-y-5 p-24">
-      <h1 className="text-4xl font-bold text-center">
-        embedding.dev
-      </h1>
-      <form className="space-x-2" onSubmit={makeApiCall}>
-        <input
-          type="text"
-          className="rounded-xl border-2 border-gray-300 p-4"
-          placeholder="enter a query"
-          value={query}
-          onChange={e => {
-            setQuery(e.target.value)
-          }}
-        />
-        <button className="rounded-xl border-2 border-gray-300 p-4 bg-gray-100">
-          submit
-        </button>
-      </form>
-      <p>
-        current dataset: United States Wikipedia Page
-      </p>
-      <div className="flex-initial">
-        {models.map((model) => (
-          <div key={model.name} className="flex gap-6 tracking-wide mb-10">
-            <div className="items-center h-max align-middle">
-              <h2 className="text-gray-500">{model.company}</h2>
-              <h1 className="font-bold text-xl text-gray-950">{model.name}</h1>
-              <a href={model.link} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
-            </div>
-            <div className="flex gap-2">
-              {outputs[model.name]?.map((output) => (
-                <div key={v4()} className="border-2 border-gray-800 rounded-xl p-2">
-                  {output.text}
-                  <div className="text-center text-gray-500 p-2">
-                    Distance: {output.distance}
+    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Header />
+      {/* <main className="flex h-screen flex-col items-center space-y-5 p-24"> */}
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
+        <form className="space-x-2" onSubmit={makeApiCall}>
+          <input
+            type="text"
+            className="rounded-xl border-2 border-gray-300 p-4"
+            placeholder="enter a query"
+            value={query}
+            onChange={e => {
+              setQuery(e.target.value)
+            }}
+          />
+          <button className="rounded-xl border-2 border-gray-300 p-4 bg-gray-100">
+            submit
+          </button>
+        </form>
+        <p>
+          current dataset: United States Wikipedia Page
+        </p>
+        <div className="flex-initial">
+          {models.map((model) => (
+            <div key={model.name} className="flex gap-6 tracking-wide mb-10">
+              <div className="items-center h-max align-middle">
+                <h2 className="text-gray-500">{model.company}</h2>
+                <h1 className="font-bold text-xl text-gray-950">{model.name}</h1>
+                <a href={model.link} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
+              </div>
+              <div className="flex gap-2">
+                {outputs[model.name]?.map((output) => (
+                  <div key={v4()} className="border-2 border-gray-800 rounded-xl p-2">
+                    {output.text}
+                    <div className="text-center text-gray-500 p-2">
+                      Distance: {output.distance}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <footer className="space-x-12 justify-self-end pb-2">
-        <a href="https://github.com/justinliang1020/embedding-dev" className="no-underline hover:underline text-blue-400">github</a>
-        <a href="https://www.trychroma.com/" className="no-underline hover:underline text-blue-400">powered by chroma</a>
-      </footer>
-    </main>
+          ))}
+        </div>
+        <footer className="space-x-12 justify-self-end pb-2">
+          <a href="https://github.com/justinliang1020/embedding-dev" className="no-underline hover:underline text-blue-400">github</a>
+          <a href="https://www.trychroma.com/" className="no-underline hover:underline text-blue-400">powered by chroma</a>
+        </footer>
+      </main>
+    </div>
   )
 }
