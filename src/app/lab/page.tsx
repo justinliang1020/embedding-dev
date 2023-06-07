4421345eb932d5814cc573a20337dba7debc4f5d
Justin Liang <justinliang1020@gmail.com>
2023-06-07 11:43:26 -0700

    front page

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -4,14 +4,14 @@ export default function Lab() {
     return (
         <div className="flex max-w-6xl mx-auto flex-col justify-center py-2 min-h-screen">
             <Header />
-
             <main className="grid grid-cols-3 flex-1 w-full items-center justify-center text-center px-4 mt-5 background-gradient">
-                <div className="col-span-1 border border-gray-800 h-full">
+                under construction...
+                {/* <div className="col-span-1 border border-gray-800 h-full">
                     hi
                 </div>
                 <div className="col-span-2 border border-gray-800 h-full">
                     hello
-                </div>
+                </div> */}
             </main>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,40 @@
-"use client";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { FormEvent, useState } from "react";
-import { v4 as uuidv4, v4 } from "uuid";
-import { Output, models } from "@/utils/utils";
-import Header from "../components/Header";
+import Header from "@/components/Header"
+import Image from "next/image"
+import UndrawScience from "../../public/undraw_science_re_mnnr.svg"
+import UndrawCompare from "../../public/undraw_split_testing_l1uw.svg"
+import Link from "next/link"
+function ModuleCard({ name, desc, image }: { name: string, desc: string, image: any }) {
+  return (
+    <Link className="card w-96 bg-base-100 shadow-xl mx-6" href={`/${name}`}>
+      <>
+        <figure className="h-60">
+          <Image src={image} alt={name} />
+        </figure>
+        <div className="card-body">
+          <div className="card-title justify-center link" >embedding.dev/{name}</div>
+          <p>{desc}</p>
+        </div>
+      </>
+    </Link>
+  )
+}
 export default function Home() {
-  const [outputs, setOutputs] = useState<{ [key: string]: Output[] }>({})
-  const [query, setQuery] = useState("");
-  const [loading, setLoading] = useState(false);
-  const makeApiCall = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setLoading(true);
-    const res = await fetch("api/query", {
-      method: "POST",
-      body: JSON.stringify({
-        query: query
-      })
-    });
-    setOutputs(await res.json());
-    setTimeout(() => {
-      setLoading(false);
-    }, 100);
-  }
   return (
     <div className="flex max-w-6xl mx-auto flex-col justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-col flex-1 w-full items-center justify-center text-center px-4 mt-5 background-gradient">
-        {/* <main className="grid grid-cols-3 h-full"> */}
-        <h1 className="font-bold sm:text-3xl text-xl pb-4">
-          Compare Embedding Models
-        </h1>
-        <div className="pb-2">
-          current document: United States Wikipedia Page
-          
+        <div>
+          <h1 className="text-3xl font-bold">
+            the toolkit to experiment with
+            <br />
+            embedding models
+            {/* <a href="https://docs.trychroma.com/embeddings" className="link font-extrabold">embedding models</a> */}
+          </h1>
+          <p>free, open-source, built on <a href="https://www.trychroma.com/" className="link">chroma</a></p>
         </div>
-        <form className="space-x-2" onSubmit={makeApiCall}>
-          <input
-            type="text"
-            className="input input-bordered"
-            placeholder="enter a query"
-            value={query}
-            onChange={e => {
-              setQuery(e.target.value)
-            }}
-          />
-          {!loading && (
-            <button className="btn">Submit</button>
-          )}
-          {loading && (
-            <button disabled className="btn align-middle">
-              <span className="loading loading-spinner loading-md"></span>
-            </button>
-          )}
-        </form>
-        <div className="flex-initial">
-          {models.map((model) => (
-            <div key={model.name} className="flex gap-6 tracking-wide mb-10">
-              <div className="items-center h-max align-middle">
-                <h2 className="text-gray-500 text-center">{model.company}</h2>
-                <h1 className="font-bold text-xl text-gray-950">{model.name}</h1>
-                <a href={model.link} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
-              </div>
-              <div className="flex gap-2">
-                {outputs[model.name]?.map((output) => (
-                  <div key={v4()} className="border-2 border-gray-800 rounded-xl p-2">
-                    {output.text}
-                    <div className="text-center text-gray-500 p-2">
-                      Distance: {output.distance}
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+        <div className="flex flex-row flex-1 w-full items-center justify-center text-center px-4 mt-5 background-gradient">
+          <ModuleCard name="lab" desc="Craft the perfect text-embedding retriever (UNDER CONSTRUCTION)" image={UndrawScience} />
+          <ModuleCard name="compare" desc="Compare multiple embedding models simultaneously" image={UndrawCompare} />
         </div>
       </main>
     </div>
